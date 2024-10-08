371e1dd9f4ff6bd42f8063f99c95fbb7c54535bb
João Guimarães <141598397+freshudo@users.noreply.github.com>
2024-10-08 02:42:58 -0300

    Delete NectarDaSerra directory

--- NectarDaSerra/src/main/java/com/mycompany/nectardaserra/exception/JuiceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.nectardaserra.exception;
-
-import javax.swing.JOptionPane;
-
-public class JuiceNotFoundException extends Exception {
-    public JuiceNotFoundException() {
-        JOptionPane.showMessageDialog(null, "Suco não encontrado", "Erro", JOptionPane.ERROR_MESSAGE);
-    }
-}
